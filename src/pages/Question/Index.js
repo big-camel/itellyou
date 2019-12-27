db37078f8ce9393f336fb2b883a58cf7cc49aae3
itellyou <owner@itellyou.com>
2019-12-27 17:28:14 +0800

    update question

--- src/pages/Question/Index.js
+++ src/pages/Question/Index.js
@@ -1,53 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'dva'
 import Link from 'umi/link'
+import InfiniteScroll from 'react-infinite-scroller'
 import { Row, Col , Tabs , List, Avatar, Icon } from 'antd'
 import Loading from '@/components/Loading'
 import Timer from '@/components/Timer'
 import styles from './Index.less'
 const TabPane = Tabs.TabPane
 
-class Index extends React.Component {
+function Index({dispatch , location:{ query } , list }){
 
-    state = {
-        limit:10,
-        offset:0,
-    }
-
-    componentWillMount(){
-        const { location:{ query } } = this.props
-        const { offset } = query
-        this.setState({
-            offset: parseInt(offset || 0)
-        })
-    }
-
-    componentDidMount(){
-        this.getListData()
-    }
-
-    getListData = () => {
-        const { dispatch } = this.props
-        const { offset , limit } = this.state
+    const [offset,setOffset] = useState(parseInt(query.offset || 0))
+    const [limit,setLimit] = useState(20)
+    const [type,setType] = useState("default")
+    const [loading,setLoading] = useState(false)
+    
+    useEffect(() => {
         dispatch({
             type:'question/list',
             payload:{
                 offset,
                 limit
             }
+        }).then(() => {
+            setLoading(false)
         })
-    }
+    },[offset, limit, dispatch])
 
-    onPageChange = page => {
-        this.setState({
-            page
-        },
-        ()=>{
-            this.getListData()
-        })
-    }
-
-    getRewardTip = item => {
+    const getRewardTip = item => {
         if(item.reward_type === 0)
             return
         let type = "积分"
@@ -57,91 +37,86 @@ class Index extends React.Component {
         return <span>悬赏 {item.reward_value} {type}</span>
     }
 
-    renderList = () => {
-        const { list,listLoading } = this.props
-        if(!list || listLoading){
-            return <Loading />
-        }
-        const { page=page || 1 , size = size || 10 , total=total || 0 } = list
-        
-        return <List 
-        pagination={{
-            onChange:this.onPageChange,
-            current:page,
-            pageSize:size,
-            total,
-            itemRender:(page,type) => {
-                let pageText = page
-                if(type === "prev")
-                    pageText = "上一页"
-                else if(type === "next")
-                    pageText = "下一页"
-                else if(type === "jump-prev" || type === "jump-next")
-                    pageText = <Icon type="ellipsis" />
-                return (
-                    <Link to={`/question?page=${page}`}>{pageText}</Link>
-                )
-            }
-        }}
-        dataSource={list.data}
-        renderItem={item => (
+    const renderItem = ({ id , tags , title , author ,...item}) => {
+        return (
             <List.Item
-            key={item.id}
+            key={id}
             >
-            <div>
                 {
-                    item.tags && item.tags.length > 0 && (
+                    tags && tags.length > 0 && (
                         <div className={styles.tags}>
                             {
-                                item.tags.map(tag => (
+                                tags.map(tag => (
                                     <Link key={tag.id} target="_blank" to={`/tag/${tag.name}`}>{tag.name}</Link>
                                 ))
                             }
                             {
-                                this.getRewardTip(item)
+                                getRewardTip(item)
                             }
                         </div>
                     )
                 }
                 <h4 className={styles.title}>
-                    <Link target="_blank" to={`/question/${item.id}`}>{item.title}</Link>
+                    <Link target="_blank" to={`/question/${id}`}>{title}</Link>
                 </h4>
                 <div className={styles.actions}>
                     <div className={styles.author}>
                         <Link to=""><Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" /></Link>
-                        <Link to="">{item.author.name}</Link>
+                        <Link to="">{author.name}</Link>
                     </div>
                     <span>发布于 <Timer time={item.created_time} /></span>
                     <span><Icon type="eye" />{item.view}</span>
                     <span><Icon type="message" />{item.answers}</span>
                     <span><Icon type="heart" /></span>
                 </div>
-            </div>
             </List.Item>
-        )}
-        />
-    }
-
-    render(){
-        return (
-            <Row style={{marginLeft:'-8px',marginRight:'-8px'}}>
-                <Col xs={24} sm={18} style={{paddingLeft:'8px',paddingRight:'8px'}}>
-                    <Tabs>
-                        <TabPane tab="最新问答" key="1">
-                            { this.renderList() }
-                        </TabPane>
-                        <TabPane tab="Tab 2" key="2">
-2
-                        </TabPane>
-                        <TabPane tab="Tab 3" key="3">
-3
-                        </TabPane>
-                    </Tabs>
-                </Col>
-                <Col xs={24} sm={6} style={{paddingLeft:'8px',paddingRight:'8px'}}>dfdfsdf</Col>
-            </Row>
         )
     }
+
+    const renderList = () => {
+        if(!list) return <Loading />
+        
+        return (
+            <InfiniteScroll
+            initialLoad={false}
+            pageStart={0}
+            loadMore={() => {
+                setLoading(true)
+                setOffset(offset + limit)
+            }}
+            hasMore={!loading && !list.end}
+            useWindow={true}
+            >
+                <List 
+                dataSource={list.data}
+                renderItem={renderItem}
+                itemLayout="vertical"
+                />
+            </InfiniteScroll>
+        )
+    }
+
+    return (
+        <Row style={{marginLeft:'-8px',marginRight:'-8px'}}>
+            <Col xs={24} sm={18} style={{paddingLeft:'8px',paddingRight:'8px'}}>
+                <Tabs>
+                    <TabPane tab="热门回答" key="1">
+                        { renderList() }
+                    </TabPane>
+                    <TabPane tab="悬赏问答" key="2">
+2
+                    </TabPane>
+                    <TabPane tab="最新问答" key="3">
+3
+                    </TabPane>
+                    <TabPane tab="我的关注" key="4">
+3
+                    </TabPane>
+                </Tabs>
+            </Col>
+            <Col xs={24} sm={6} style={{paddingLeft:'8px',paddingRight:'8px'}}>dfdfsdf</Col>
+        </Row>
+    )
 }
 export default connect(({ question ,loading }) => ({
     list:question.list,
